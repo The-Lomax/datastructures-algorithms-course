d588c145ae4aece3c2c0147d17d5b7df9cf34b34
The-Lomax <kryniubest@gmail.com>
2022-01-28 12:58:02 +0800

    course update

---
+++ problemSolving.js
@@ -0,0 +1,11 @@
+// Task. write a function, which takes two numbers and returns their sum.
+
+// tip: use problemSolvingApproachTips.txt for reference.
+
+// 1. Return a sum of two arguments
+// 2. inputs: two numbers: a, b. what about the number constraints? data format bounds? adding more or less numbers than two?
+// 3. sum of two numbers, int or float
+// 4. yes
+// 5. result
+
+const sum = (a, b) => {return a + b}
